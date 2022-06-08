c626099be4ec823e0f180741f2dcd47a88ead027
Tae Hyeon Jo <hillskuti@naver.com>
2022-06-08 16:58:32 +0900

    포켓몬 이미지 랜덤 배열 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
 import './App.css';
+import { useState } from 'react';
+
+// 이미지 파일은 변경되지 않으므로 Component 밖에서 const로 선언
+const pokeCardImages = [
+  { src: 'img/purin.jpg' },
+  { src: 'img/hasi.jpg' },
+  { src: 'img/marin.jpg' },
+  { src: 'img/pairi.jpg' },
+  { src: 'img/pantem.jpg' },
+  { src: 'img/picacu.jpg' },
+];
 
 function App() {
+  // 포켓몬 카드 State
+  const [pokeCards, setPokeCards] = useState([]);
+  // 시도 횟수 State
+  const [turns, setTurns] = useState(0);
+
+  // 포켓몬 카드순서 랜덤 변경
+  const mixCards = () => {
+    // ... Spread Operater를 한번만 사용한다면, pokeCardImages와 mixedcards는 사실상 같은 값이된다.
+    const mixedCards = [...pokeCardImages, ...pokeCardImages]
+      // sort 메소드는 function의 return 값이 0보다 작으면 변경사항이 없다.
+      // 반면, 0보다 큰 숫자가 오면 숫자가 변경되게 된다.
+      .sort(() => {
+        return Math.random() - 0.5;
+      })
+      .map((card) => {
+        return { ...card, id: Math.random() };
+      });
+
+    // 포켓몬카드 순서 변경
+    setPokeCards(mixedCards);
+    // 시도 횟수 초기화
+    setTurns(0);
+  };
+
+  console.log(pokeCards, turns);
+
   return (
     <div className='App'>
       <h1>포켓몬 같은그림 찾기 게임</h1>
-      <button>새 게임</button>
+      <button onClick={mixCards}>새 게임</button>
+
+      <div>
+        {pokeCards.map((pokeCard) => {
+          return <img src={pokeCard.src} alt='' />;
+        })}
+      </div>
     </div>
   );
 }
